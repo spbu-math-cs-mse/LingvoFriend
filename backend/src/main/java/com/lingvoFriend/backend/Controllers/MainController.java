a08fe8c49b9e47b8c3f3cbbf299ac21805c68d64
Кирилл Миронов <k.mironov03@gmail.com>
2024-11-18 23:13:32 +0300

    added message roles support

--- backend/src/main/java/com/lingvoFriend/backend/Controllers/MainController.java
+++ backend/src/main/java/com/lingvoFriend/backend/Controllers/MainController.java
@@ -9,8 +9,6 @@ import com.lingvoFriend.backend.Services.ChatService.models.Message;
 
 import io.github.cdimascio.dotenv.Dotenv;
 
-import lombok.AllArgsConstructor;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -19,20 +17,39 @@ import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
+import java.util.Objects;
 
 @RestController
-@AllArgsConstructor
 @RequestMapping("/api")
 class Controller {
 
     private final LlmRequest llmRequest;
     private final ChatService chatService;
+    private final String MODEL_URI;
+    private final String API_KEY;
+    private final String FOLDER_ID;
+
+    public Controller(LlmRequest llmRequest, ChatService chatService) {
+        this.llmRequest = llmRequest;
+        this.chatService = chatService;
+
+        Dotenv dotenv = Dotenv.load();
+        this.MODEL_URI = dotenv.get("MODEL_URI");
+        this.API_KEY = dotenv.get("API_KEY");
+        this.FOLDER_ID = dotenv.get("FOLDER_ID");
+    }
 
     @PostMapping("/llm")
     public Mono<ResponseEntity<String>> sendRequestToLLM(@RequestBody LlmRequestDto llmRequestDto)
             throws JsonProcessingException {
 
         Dotenv dotenv = Dotenv.load();
+
+        if (!(Objects.equals(llmRequestDto.getMessage().getRole(), "user")
+                || (Objects.equals(llmRequestDto.getMessage().getRole(), "system")))) {
+            return Mono.just(
+                    ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Bad message role"));
+        }
 
         return Mono.defer(
                         () -> {
@@ -41,16 +58,21 @@ class Controller {
                                             llmRequestDto.getUsername(),
                                             llmRequestDto.getMessage());
 
+                            if (Objects.equals(llmRequestDto.getMessage().getRole(), "system")) {
+                                return Mono.just(
+                                        ResponseEntity.status(HttpStatus.CREATED)
+                                                .body("System message successfully saved"));
+                            }
+
                             LlmRequestModel request =
                                     new LlmRequestModel(
-                                            System.getenv("MODEL_URI"),
+                                            MODEL_URI,
                                             new LlmRequestModel.CompletionOptions(
                                                     false, 0.6, "2000"),
                                             updatedMessages);
 
                             return llmRequest
-                                    .sendPostRequest(
-                                            dotenv.get("API_KEY"), dotenv.get("FOLDER_ID"), request)
+                                    .sendPostRequest(API_KEY, FOLDER_ID, request)
                                     .map(
                                             res -> {
                                                 Message msg =
