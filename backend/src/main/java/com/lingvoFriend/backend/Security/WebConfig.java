cdd1f62b9eb5c9446ed70b965ae19569cb3b568e
Кирилл Миронов <k.mironov03@gmail.com>
2024-11-30 16:41:50 +0300

    another fix

--- backend/src/main/java/com/lingvoFriend/backend/Security/WebConfig.java
+++ backend/src/main/java/com/lingvoFriend/backend/Security/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://lingvofriend.fun:3000")
+                .allowedOrigins("http://localhost:3000", "https://lingvofriend.fun")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
