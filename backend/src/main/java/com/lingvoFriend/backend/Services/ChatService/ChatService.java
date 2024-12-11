7c2e329aaf7cb849183ec986d227ef4168270900
Kirill Who <144121860+kirillidk@users.noreply.github.com>
2024-12-11 08:44:34 +0000

    chat-history-bug (#91)
    Co-authored-by: Akhunzianov Renat <94622739+Akhunzianov@users.noreply.github.com>

--- backend/src/main/java/com/lingvoFriend/backend/Services/ChatService/ChatService.java
+++ backend/src/main/java/com/lingvoFriend/backend/Services/ChatService/ChatService.java
@@ -3,21 +3,29 @@ package com.lingvoFriend.backend.Services.ChatService;
 import com.lingvoFriend.backend.Services.AuthService.models.UserModel;
 import com.lingvoFriend.backend.Services.ChatService.dto.UserMessageDto;
 import com.lingvoFriend.backend.Services.ChatService.models.Message;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ChatService {
+    @Autowired private UserService userService;
+    @Autowired private LlmService llmService;
+    @Autowired private LanguageLevelService languageLevelService;
+    private LlmReminderService llmReminderService;
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+    private final Integer remainderFrequency = 25;
+
     public String chat(UserMessageDto userMessageDto) {
         UserModel user = userService.findOrThrow(userMessageDto.getUsername());
         userService.addMessageToUser(user, userMessageDto.getMessage());
 
-        if (userMessageDto.getMessage().isSystem())
-            return "successfully added system message";
+        if (userMessageDto.getMessage().isSystem()) return "successfully added system message";
 
         Message llmMessage = generateResponseImpl(user);
         userService.addMessageToUser(user, llmMessage);
@@ -26,12 +34,15 @@ public class ChatService {
 
     public List<Message> getHistory(String username) {
         UserModel user = userService.findOrThrow(username);
-        return user.getMessages();
+        return user.getMessages().stream()
+                .filter(message -> !message.isSystem())
+                .collect(Collectors.toList());
     }
 
     private Message generateResponseImpl(UserModel user) {
         if (!languageLevelService.isEvaluated(user)) {
-            logger.info("User {} is not evaluated. Delegating to LanguageLevelService", user.getName());
+            logger.info(
+                    "User {} is not evaluated. Delegating to LanguageLevelService", user.getName());
             return languageLevelService.evaluate(user);
         }
         if (user.getMessages().size() % remainderFrequency == 0) {
@@ -39,14 +50,4 @@ public class ChatService {
         }
         return llmService.generateLlmResponse(user);
     }
-
-    @Autowired
-    private UserService userService;
-    @Autowired
-    private LlmService llmService;
-    @Autowired
-    private LanguageLevelService languageLevelService;
-    private LlmReminderService llmReminderService;
-    private final Logger logger = LoggerFactory.getLogger(getClass());
-    private final Integer remainderFrequency = 25;
 }
