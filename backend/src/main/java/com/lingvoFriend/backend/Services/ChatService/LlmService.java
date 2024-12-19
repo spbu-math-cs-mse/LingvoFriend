8b96582c7c418430778490ab48b9a7b46be0cf56
Danila Usachev <duesna897@gmail.com>
2024-12-19 21:20:48 +0300

    LlmService mock

--- backend/src/main/java/com/lingvoFriend/backend/Services/ChatService/LlmService.java
+++ backend/src/main/java/com/lingvoFriend/backend/Services/ChatService/LlmService.java
@@ -19,44 +19,45 @@ public class LlmService {
 
     public LlmService() {
         Dotenv dotenv = Dotenv.load();
-        this.apiKey = dotenv.get("API_KEY");
-        this.folderID = dotenv.get("FOLDER_ID");
-        this.modelURI = dotenv.get("MODEL_URI");
+//        this.apiKey = dotenv.get("API_KEY");
+//        this.folderID = dotenv.get("FOLDER_ID");
+//        this.modelURI = dotenv.get("MODEL_URI");
     }
 
-    public LlmResponseModel sendPostRequest(LlmRequestModel requestModel) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Content-Type", "application/json");
-        headers.set("Authorization", "Api-key " + apiKey);
-        headers.set("x-folder-id", folderID);
-
-        HttpEntity<LlmRequestModel> entity = new HttpEntity<>(requestModel, headers);
-
-        ResponseEntity<LlmResponseModel> response =
-            restTemplate.exchange(
-                "https://llm.api.cloud.yandex.net/foundationModels/v1/completion",
-                HttpMethod.POST,
-                entity,
-                LlmResponseModel.class);
-
-        return response.getBody();
-    }
+//    public LlmResponseModel sendPostRequest(LlmRequestModel requestModel) {
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.set("Content-Type", "application/json");
+//        headers.set("Authorization", "Api-key " + apiKey);
+//        headers.set("x-folder-id", folderID);
+//
+//        HttpEntity<LlmRequestModel> entity = new HttpEntity<>(requestModel, headers);
+//
+//        ResponseEntity<LlmResponseModel> response =
+//            restTemplate.exchange(
+//                "https://llm.api.cloud.yandex.net/foundationModels/v1/completion",
+//                HttpMethod.POST,
+//                entity,
+//                LlmResponseModel.class);
+//
+//        return response.getBody();
+//    }
 
     public Message generateLlmResponse(UserModel user) {
-        LlmRequestModel request =
-            new LlmRequestModel(
-                modelURI,
-                new LlmRequestModel.CompletionOptions(false, 0.6, "2000"),
-                user.getMessages());
-        LlmResponseModel response = sendPostRequest(request);
-        if (response == null || response.getResult() == null)
-            throw new IllegalStateException("LLM returned empty response");
-        return response.getResult().getAlternatives().get(0).getMessage();
+//        LlmRequestModel request =
+//            new LlmRequestModel(
+//                modelURI,
+//                new LlmRequestModel.CompletionOptions(false, 0.6, "2000"),
+//                user.getMessages());
+//        LlmResponseModel response = sendPostRequest(request);
+//        if (response == null || response.getResult() == null)
+//            throw new IllegalStateException("LLM returned empty response");
+//        return response.getResult().getAlternatives().get(0).getMessage();
+        return new Message("assistant", "MOCK LLM RESPONSE");
     }
 
     @Autowired
     private RestTemplate restTemplate;
-    private final String apiKey;
-    private final String folderID;
-    private final String modelURI;
+//    private final String apiKey;
+//    private final String folderID;
+//    private final String modelURI;
 }
