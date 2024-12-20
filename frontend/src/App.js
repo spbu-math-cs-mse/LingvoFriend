8c650f0ca6aaac53950652593f12401b48ebff94
Кирилл Миронов <k.mironov03@gmail.com>
2024-12-20 21:06:51 +0300

    auto-auth-in-miniapp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,8 +15,66 @@ import Questionnaire from "./components/pages/questionnaire/Questionnaire";
 import useAuth from "./components/authForm/useAuth";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
+    const [isMiniApp, setIsMiniApp] = useState(false);
+
+    useEffect(() => {
+        const checkIsMiniApp = async () => {
+            if (window.TelegramWebviewProxy) {
+                setIsMiniApp(true);
+
+                const serverUrl = process.env.REACT_APP_SERVER_URL || "";
+                try {
+                    const response = await fetch(
+                        `${serverUrl}/api/auth/login`,
+                        {
+                            method: "POST",
+                            headers: {
+                                "Content-Type": "application/json",
+                            },
+                            credentials: "include",
+                            body: JSON.stringify({
+                                username: "who",
+                                password: "pass",
+                            }),
+                        }
+                    );
+
+                    if (response.ok) {
+                        window.location.href = "/home";
+                    } else {
+                        const errorData = await response.json();
+                        if (
+                            response.status === 400 &&
+                            errorData.errorMessage === "WRONG_USERNAME"
+                        ) {
+                            toast.error("Неправильное имя пользователя");
+                        } else if (
+                            response.status === 401 &&
+                            errorData.errorMessage === "WRONG_PASSWORD"
+                        ) {
+                            toast.error("Неправильный пароль");
+                        } else {
+                            toast.error(
+                                "Произошла ошибка. Пожалуйста, попробуйте снова."
+                            );
+                        }
+                    }
+                } catch (error) {
+                    toast.error(
+                        "Что-то пошло не так. Проверьте соединение с интернетом."
+                    );
+                }
+            }
+        };
+
+        checkIsMiniApp();
+    }, []);
+
     const PrivateRoute = ({ children }) => {
         const isAuthenticated = useAuth();
 
@@ -39,6 +97,13 @@ function App() {
 
     return (
         <Router>
+            <div>
+                {isMiniApp ? (
+                    <h1>hey, it's Mini App!</h1>
+                ) : (
+                    <h1>hey, it's Browser Version!</h1>
+                )}
+            </div>
             <Routes>
                 <Route
                     path="/"
